f9f3b43ce016612780551a02a2316dce8021c47b
fraguada <fraguada@gmail.com>
2024-04-18 11:24:53 +0200

    updating some files

--- samples/part1/TestingNuGet_RestSharp.cs
+++ samples/part1/TestingNuGet_RestSharp.cs
@@ -6,7 +6,7 @@ using Rhino;
 using RestSharp;
 using RestSharp.Authenticators;
 
-var client = new RestClient("http://httpbin.org");
+var client = new RestClient("https://httpbin.org");
 var request = new RestRequest("get", DataFormat.Json);
 var response = client.Get(request);
 
